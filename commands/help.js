bac2d66711f445c1e0d0e428943620fc49003cbb
ben <ben.ng@outlook.com>
2019-09-07 02:25:29 -0400

    updates things. i've lost track now

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'List all of my commands or info about a specific command',
     aliases: ['commands'],
     usage: '<command_name>',
-    cooldown: 5,
+    cooldown: 3,
     execute(message, args) {
         let data = [];
         const { commands } = message.client;
