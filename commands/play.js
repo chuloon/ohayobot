c93e419cb9677fc37a13c365ec6ca56f8fe75cd3
ben <ben.ng@outlook.com>
2019-09-07 01:05:52 -0400

    added csgo restriction

--- commands/play.js
+++ commands/play.js
@@ -1,12 +1,13 @@
 module.exports = {
     name: 'play',
     description: 'Adds a player to a game',
-    usage: '<game_name1> <game_name2> ...',
+    usage: '<game_name1> <game_name2> ...\nPossible game names: overwatch, league, tft, csgo',
     execute(message, args) {
         args.forEach((arg) => {
             arg = arg.toLowerCase();
             if(arg == "league") arg = "league-of-legends";
             else if(arg == "tft") arg = "teamfight-tactics";
+            else if(arg == "csgo") arg = "counter-strike";
             const gameRole = getRole(message.guild.roles, arg);
             if(!gameRole) throw e;
 
