df7124b3b437639647913c4c6180021f3b681dcb
Ben Ng <ben.ng@outlook.com>
2020-09-16 11:59:53 -0400

    removed test embeded message

--- index.js
+++ index.js
@@ -8,22 +8,6 @@ const { prefix, token } = require('./config.json');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const embedMessage = new Discord.RichEmbed()
-    .setColor('#0099ff')
-    .setTitle('Some title')
-    .setURL('https://discord.js.org/')
-    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-    .setDescription('Some description here')
-    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-    .addField('Regular field title', 'Some value here')
-    .addBlankField()
-    .addField('Inline field title', 'Some value here', true)
-    .addField('Inline field title', 'Some value here', true)
-    .addField('Inline field title', 'Some value here', true)
-    .setImage('https://i.imgur.com/wSTFkRM.png')
-    .setTimestamp()
-    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
 commandFiles.forEach((file) => {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
